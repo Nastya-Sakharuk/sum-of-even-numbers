859f8740cc33769bb19a1694624cee00d76b851a
Anastasiya Sakharuk <sakharuk333@gmail.com>
2022-11-23 22:21:00 +0300

    Sum of even numbers

--- src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
+++ src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
@@ -4,14 +4,19 @@ public class SumOfEvenNumbers {
 
     public static void main(String[] args) {
         int[] array = new int[]{1, 3, 2, 8, 15, 199};
-
         System.out.println(sum(array));
     }
 
-    public static int sum(int[] array){
-
-        //put your code here
-
-        throw new UnsupportedOperationException();
+    public static int sum(int[] array) {
+        int sum = 0;
+        if (array == null) {
+            return 0;
+        }
+        for (int elem : array) {
+            if (elem % 2 == 0) {
+                sum = sum + elem;
+            }
+        }
+        return sum;
     }
 }
